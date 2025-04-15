d98e55fde54c6f451988d411ce8afcc1d492401b
sajid15 <sajid@enterprisebot.org>
2025-04-15 13:49:36 +0530

    added module federation to cra app

--- crahost/src/App.js
+++ crahost/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { lazy, Suspense } from 'react';
+const RemoteApp = lazy(() => import("remoteA/remoteAapp"));
+const RemoteBapp = lazy(()=> import("remoteB/remoteBapp"));
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRA HOST APP</h1>
+      <Suspense fallback={<p>loading....</p>}>
+          <RemoteApp success={true} />
+          <RemoteBapp success={false} />
+      </Suspense>
     </div>
   );
 }
